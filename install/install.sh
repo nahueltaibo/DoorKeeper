19e5b4e12c19e55516f4b330aab1008fd8b9e2b7
nahueltaibo <nahueltaibo@gmail.com>
2019-05-29 23:36:43 -0400

    Adding packates to install

--- install/install.sh
+++ install/install.sh
@@ -10,3 +10,7 @@ then
 else
   sed -i '1s;^;deb http://www.linux-projects.org/listing/uv4l_repo/raspbian/stretch stretch main\n;' /etc/apt/sources.list
 fi
+
+sudo apt-get update && sudo apt-get install \
+  uv4l \
+  uv4l-raspicam
