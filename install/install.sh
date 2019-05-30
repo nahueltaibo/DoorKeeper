7df6d3b302f5a16d85242b6a86be57d66b5a6e93
nahueltaibo <nahueltaibo@gmail.com>
2019-05-30 00:23:18 -0400

    And new lines..

--- install/install.sh
+++ install/install.sh
@@ -3,10 +3,10 @@
 GREEN='\033[0;32m'
 NC='\033[0m' #No Color
 
-printf "${GREEN}Installing Door Keeper environment...${NC}"
+printf "${GREEN}Installing Door Keeper environment...${NC}\n"
 curl http://www.linux-projects.org/listing/uv4l_repo/lpkey.asc | sudo apt-key add -
 
-printf "\n${GREEN}Configuring uv4l package sources...${NC}"
+printf "\n${GREEN}Configuring uv4l package sources...${NC}\n"
 
 if grep -Fxq "deb http://www.linux-projects.org/listing/uv4l_repo/raspbian/stretch stretch main" /etc/apt/sources.list
 then
@@ -15,10 +15,10 @@ else
   sed -i '1s;^;deb http://www.linux-projects.org/listing/uv4l_repo/raspbian/stretch stretch main\n;' /etc/apt/sources.list
 fi
 
-printf "\n${GREEN}Updating and Upgrading the system...${NC}"
+printf "\n${GREEN}Updating and Upgrading the system...${NC}\n"
 sudo apt-get update && apt-get upgrade -y
 
-printf "\n${GREEN}Installing required packages...${NC}"
+printf "\n${GREEN}Installing required packages...${NC}\n"
 sudo sudo apt-get install -y \
   uv4l \
   uv4l-raspicam \
