d30c644a92fe9584d845ec6a07c0d06b91b1ebfd
nahueltaibo <nahueltaibo@gmail.com>
2019-05-29 23:29:31 -0400

    q

--- install/install.sh
+++ install/install.sh
@@ -8,3 +8,9 @@ curl http://www.linux-projects.org/listing/uv4l_repo/lpkey.asc | sudo apt-key ad
 if grep -q "deb http://www.linux-projects.org/listing/uv4l_repo/raspbian/stretch stretch main" "$File"; == 1 then
   sed -i '1s;^;deb http://www.linux-projects.org/listing/uv4l_repo/raspbian/stretch stretch main\n;' /etc/apt/sources.list
 fi
+
+if grep -Fxq "deb http://www.linux-projects.org/listing/uv4l_repo/raspbian/stretch stretch main" /etc/apt/sources.list
+then
+else
+  sed -i '1s;^;deb http://www.linux-projects.org/listing/uv4l_repo/raspbian/stretch stretch main\n;' /etc/apt/sources.list
+fi
