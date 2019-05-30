23e0fa9d467751e29733e92fa00c8ffd4d31d7c5
nahueltaibo <nahueltaibo@gmail.com>
2019-05-30 00:16:35 -0400

    Added more details on each step

--- install/install.sh
+++ install/install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 echo "Insalling Door Keeper environment.."
-
 curl http://www.linux-projects.org/listing/uv4l_repo/lpkey.asc | sudo apt-key add -
 
+echo " "
+echo Configuring uv4l package sources...
 if grep -Fxq "deb http://www.linux-projects.org/listing/uv4l_repo/raspbian/stretch stretch main" /etc/apt/sources.list
 then
   echo "uv4l url already in sources.list" 
@@ -11,7 +12,13 @@ else
   sed -i '1s;^;deb http://www.linux-projects.org/listing/uv4l_repo/raspbian/stretch stretch main\n;' /etc/apt/sources.list
 fi
 
-sudo apt-get update && sudo apt-get install \
+echo " "
+echo Updating and Upgrading the system...
+sudo apt-get update && apt-get upgrade -y
+
+echo " "
+echo Installing required packages...
+sudo sudo apt-get install -y \
   uv4l \
   uv4l-raspicam \
   uv4l-raspicam-extras \
