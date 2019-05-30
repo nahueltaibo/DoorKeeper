671138d81a399d48bbffa9a256e028c2d9183435
nahueltaibo <nahueltaibo@gmail.com>
2019-05-29 23:33:04 -0400

    a

--- install/install.sh
+++ install/install.sh
@@ -5,10 +5,6 @@ echo "Insalling Door Keeper environment.."
 echo "text here" >> filename
 curl http://www.linux-projects.org/listing/uv4l_repo/lpkey.asc | sudo apt-key add -
 
-if grep -q "deb http://www.linux-projects.org/listing/uv4l_repo/raspbian/stretch stretch main" "$File"; == 1 then
-  sed -i '1s;^;deb http://www.linux-projects.org/listing/uv4l_repo/raspbian/stretch stretch main\n;' /etc/apt/sources.list
-fi
-
 if grep -Fxq "deb http://www.linux-projects.org/listing/uv4l_repo/raspbian/stretch stretch main" /etc/apt/sources.list
 then
 else
