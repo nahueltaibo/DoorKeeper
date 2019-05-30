376c6efd441dc72ace4d4e3b42c38730eeaeeccb
nahueltaibo <nahueltaibo@gmail.com>
2019-05-29 23:34:19 -0400

    a

--- install/install.sh
+++ install/install.sh
@@ -2,11 +2,11 @@
 
 echo "Insalling Door Keeper environment.."
 
-echo "text here" >> filename
 curl http://www.linux-projects.org/listing/uv4l_repo/lpkey.asc | sudo apt-key add -
 
 if grep -Fxq "deb http://www.linux-projects.org/listing/uv4l_repo/raspbian/stretch stretch main" /etc/apt/sources.list
 then
+  echo "uv4l url already in sources.list" 
 else
   sed -i '1s;^;deb http://www.linux-projects.org/listing/uv4l_repo/raspbian/stretch stretch main\n;' /etc/apt/sources.list
 fi
