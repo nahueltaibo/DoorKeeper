c41880fe4bab23360e7386c927bc4360a9210e20
nahueltaibo <nahueltaibo@gmail.com>
2019-05-30 00:22:31 -0400

    Adding colors to the script output.

--- install/install.sh
+++ install/install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-echo "Insalling Door Keeper environment.."
+GREEN='\033[0;32m'
+NC='\033[0m' #No Color
+
+printf "${GREEN}Installing Door Keeper environment...${NC}"
 curl http://www.linux-projects.org/listing/uv4l_repo/lpkey.asc | sudo apt-key add -
 
-echo " "
-echo Configuring uv4l package sources...
+printf "\n${GREEN}Configuring uv4l package sources...${NC}"
+
 if grep -Fxq "deb http://www.linux-projects.org/listing/uv4l_repo/raspbian/stretch stretch main" /etc/apt/sources.list
 then
   echo "uv4l url already in sources.list" 
@@ -12,12 +15,10 @@ else
   sed -i '1s;^;deb http://www.linux-projects.org/listing/uv4l_repo/raspbian/stretch stretch main\n;' /etc/apt/sources.list
 fi
 
-echo " "
-echo Updating and Upgrading the system...
+printf "\n${GREEN}Updating and Upgrading the system...${NC}"
 sudo apt-get update && apt-get upgrade -y
 
-echo " "
-echo Installing required packages...
+printf "\n${GREEN}Installing required packages...${NC}"
 sudo sudo apt-get install -y \
   uv4l \
   uv4l-raspicam \
